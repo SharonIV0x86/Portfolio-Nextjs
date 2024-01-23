941babe3989239198223b032b6b5ecdc3b5f19e0
SharonIV0X86 <anirudhlakhanpal23@gmail.com>
2024-01-23 16:29:26 +0530

    title changed

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
     <div>
       <Head>
         <title>Portfolio | Front-End Developer</title>
-        <meta name="description" content="I’m a front-end web developer specializing in building (and occasionally designing) exceptional digital experiences." />
+        <meta name="description" content="I’m a Web Developer specializing in building (and occasionally designing) exceptional digital experiences." />
         <link rel="icon" href='../public/assets/AL.png' />
       </Head>
     <Main />
